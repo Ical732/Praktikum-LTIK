b4b5aa94f36d257c219db34a1478e34ca337258f
Muhammad Faisal Anwar <mhmmdfaisalanwar@student.upi.edu>
2025-11-05 23:25:10 +0700

    Menambahkan file Kalkulator sederhana.py

---
+++ Kalkulator sederhana.py
@@ -0,0 +1,12 @@
+"""Muhammad Faisal Anwar
+   NIM: 2512458
+   Kelas: 1A"""
+
+print("Input nilai a, b, dan c untuk dijumlahkan!")
+
+a = int(input("Input a = "))
+b = int(input("Input b = "))
+c = int(input("Input c = "))
+jumlah = a + b + c
+
+print(f"Hasil dari penjumlahan {a} + {b} + {c} = {jumlah}")
